27982292db44178c531d8d3042c0510b7bdbfd5c
Markian Rutkowskyj <i-keep-laughing@myself.com>
2024-07-20 12:08:49 +0100

    Updated to Maven and java version

--- src/main/java/com/JfXAmp/Main.java
+++ src/main/java/com/JfXAmp/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("View/MainWindow.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/MainWindow.fxml"));
 
         primaryStage.setTitle("JFXAmp - V0.1 - Markian Rutkowskyj");
         primaryStage.setScene(new Scene(root, 800, 500));
@@ -19,9 +19,6 @@ public class Main extends Application {
         primaryStage.setMinWidth(400);
 
         primaryStage.show();
-
-
-
     }
 
 
--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module JFXAmp {
-    requires javafx.fxml;
-    requires javafx.controls;
-    requires javafx.graphics;
-    requires javafx.web;
-    requires javafx.media;
-
-    opens com.JfXAmp;
-}
